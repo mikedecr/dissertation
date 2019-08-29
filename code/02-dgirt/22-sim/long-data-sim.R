710750933374ac9f5827366004d7af9750c448f8
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2019-08-29 10:48:41 -0500

    params as objs, refactor HLM, ggplot failsafes
    - saving parameter values as objects for easier rmd
    - accidentally forgot a state error term in simulation
    - create district data as separate from group data
    - some stopchecks for number of covs at each level (1 state, 2 dist)
    - decide whether to plot using if (), not try()
    - DISCRIMINATION is identified (should this be in there?)

--- code/02-dgirt/22-sim/long-data-sim.R
+++ code/02-dgirt/22-sim/long-data-sim.R
@@ -3,6 +3,8 @@
 #   Creating "long" data for long (non-matrix) model
 # ----------------------------------------------------
 
+
+
 # source locally:
 # source(here::here("code", "02-dgirt", "22-sim", "long-data-sim.R"))
 
@@ -45,39 +47,76 @@ if (whoami == "decrescenzo") {
 # 40 items, normal cutpoints, lognormal districtionation
 # y_{ij} is normal ogive model
 
-set.seed(7231946)
+seed_val <- 7231946
+set.seed(seed_val)
 
+# --- saved params for simulated data --- 
+
+# should this all just be consolidated into one thing?
+# arg for no: we need some party-specific things
+# maybe just split them up and then rm()?
+
+# indexing (this can't go anywhere else)
 n_regions <- 5
 n_states <- 20
 n_districts <- 5
 n_parties <- 2
 n_cases <- 50
 n_items <- 40
-# will these do anything
-n_statecov <- 1
-n_distcov <- 2
+# will these do anything? (could sim from mvnorm?)
+n_statecov <- 1L
+n_distcov <- 2L
+
+# hierarchical model: mean
+const_r <- 1
+const_d <- -1
+sd_beta_d <- 0.25
+sd_beta_s <- 0.25
+resid_theta_d <- 1
+resid_theta_s <- 0.1
+resid_theta_r <- 0.1
+
+# hierarchical model: sigma
+const_sig <- 0.125
+sd_beta_d_sig <- 0.25
+sd_beta_s_sig <- 0.25
+resid_sigma_d <- 0.5
+resid_sigma_s <- 0.1
+resid_sigma_r <- 0.1
+
+# item parameters
+difficulty_sd <- 0.1
+discrimination_sd <- 0.35
+
+
+
 
 params <- 
   tibble(party = c(1, 2)) %>%
   group_by(party) %>%
   mutate(
     params = map(
-      1, ~ tibble(# theta hypermean 
-                  const = ifelse(party == 1, -1, 1),
-                  beta_state = rnorm(.x, mean = 0, sd = 0.25), 
-                  beta_d1 = rnorm(.x, mean = 0, sd = 0.25),
-                  beta_d2 = rnorm(.x, mean = 0, sd = 0.25),
-                  # sigma hypermean
-                  const_sig = rnorm(.x, mean = 0, sd = 0.125),
-                  beta_sig_state = rnorm(.x, mean = 0, sd = 0.25), 
-                  beta_sig_d1 = rnorm(.x, mean = 0, sd = 0.25),
-                  beta_sig_d2 = rnorm(.x, mean = 0, sd = 0.25),
-                  # residual variances after hypermeans
-                  sd_theta = 1,
-                  sd_sigma = 0.25)
-    )
+      1, ~ tibble(
+        # theta hypermean 
+        const = ifelse(party == 1, const_d, const_r),
+        beta_d1 = rnorm(.x, mean = 0, sd = sd_beta_d),
+        beta_d2 = rnorm(.x, mean = 0, sd = sd_beta_d),
+        beta_state = rnorm(.x, mean = 0, sd = sd_beta_s), 
+        sd_theta_d = resid_theta_d, 
+        sd_theta_s = resid_theta_s,
+        sd_theta_r = resid_theta_r,
+        # sigma hypermean
+        const_sig = rnorm(.x, mean = 0, sd = const_sig),
+        beta_sig_d1 = rnorm(.x, mean = 0, sd = sd_beta_d_sig),
+        beta_sig_d2 = rnorm(.x, mean = 0, sd = sd_beta_d_sig),
+        beta_sig_state = rnorm(.x, mean = 0, sd = sd_beta_s_sig), 
+        sd_sigma_d = resid_sigma_d,
+        sd_sigma_s = resid_sigma_s,
+        sd_sigma_r = resid_sigma_r
+      ) # end tibble
+    ) # end map
   ) %>%
-  unnest() %>%
+  unnest(params) %>%
   print()
 
 
@@ -88,106 +127,166 @@ region_level <-
   tibble(region = 1:n_regions) %>%
   crossing(party = 1:n_parties) %>%
   mutate(
-    region_effect_mean = rnorm(n(), mean = 0, sd = 0.1), 
-    region_effect_var = rnorm(n(), mean = 0, sd = 0.1)
+    region_resid_theta = rnorm(n(), mean = 0, sd = params$sd_theta_r), 
+    region_resid_sigma = rnorm(n(), mean = 0, sd = params$sd_sigma_r)
   ) %>%
   print()
 
 
-# ---- state data -----------------------
+# ---- state data and offsets-----------------------
 
-# 50 states with Z covariate
+# 50 states with Z ~ N(0, 1) covariate
 state_level <- 
-  tibble(state = 1:n_states,
-         Z = rnorm(n_states)) %>%
+  tibble(
+    state = 1:n_states,
+    Z = rnorm(n_states, mean = 0, sd = 1)
+  ) %>%
+  crossing(party = c(1, 2)) %>%
+  mutate(
+    state_resid_theta = 
+      rnorm(n(), mean = 0, sd = params$sd_theta_s),
+    state_resid_sigma = 
+      rnorm(n(), mean = 0, sd = params$sd_sigma_s)
+  ) %>%
   print()
 
+state_level %>%
+  select(starts_with("Z")) %>%
+  ncol() %>%
+  (function(x) x == n_statecov) %>%
+  stopifnot()
 
-# ---- district data -----------------------
 
-# districts in each state, X covariates
-# specify group hypermeans using clever indexing
-group_level <- 
-  crossing(state = 1:n_states,
-           district = 1:n_districts, 
-           party = 1:n_parties) %>%
+# ---- district data (no offsets) -----------------------
+
+# combine regions, states, and districts
+district_level <- 
+  crossing(state = (1:n_states), district = (1:n_districts)) %>%
   mutate(region = (state %% 5) + 1) %>%
-  left_join(region_level) %>%
-  mutate(group = 1:n(),
-         X1 = rnorm(n()),
-         X2 = rnorm(n())) %>%
-  left_join(state_level) %>%
   mutate(
+    X1 = rnorm(n = n(), mean = 0, sd = 1),
+    X2 = rnorm(n = n(), mean = 0, sd = 1)
+  ) %>%
+  print()
+
+district_level %>%
+  select(starts_with("X")) %>%
+  ncol() %>%
+  (function(x) x == n_distcov) %>%
+  stopifnot()
+
+
+# ---- create group data -----------------------
+
+# expand district into two parties (groups)
+# calculate group hypermeans
+# - district data, state data
+# - state errors, region errors
+# - add group errors
+# exponentiate the sigma 
+group_level <- district_level %>%
+  crossing(party = c(1, 2)) %>%
+  left_join(state_level) %>%
+  left_join(region_level) %>%
+  mutate(
+    group = 1:n(),
     theta_hypermean = 
       params$const[party] + 
         (X1 * params$beta_d1[party]) + (X2 * params$beta_d2[party]) +
         (Z * params$beta_state[party]) +
-        region_effect_mean,
+        state_resid_theta + region_resid_theta,
       sigma_hypermean = 
         params$const_sig[party] + 
         (X1 * params$beta_sig_d1[party]) + (X2 * params$beta_sig_d2[party]) +
-        (Z * params$beta_sig_state[party]) + 
-        region_effect_var,
+        (Z * params$beta_sig_state[party]) +
+        state_resid_sigma + region_resid_sigma,
       theta_g = 
-        rnorm(n = n(), mean = theta_hypermean, sd = params$sd_theta),
+        rnorm(n = n(), mean = theta_hypermean, sd = params$sd_theta_d),
       sigma_g = 
-        rnorm(n = n(), mean = sigma_hypermean, sd = params$sd_sigma) %>%
-        exp()
+        exp(rnorm(n = n(), mean = sigma_hypermean, sd = params$sd_sigma_d))
   ) %>%
   print()
 
 
-# hypermeans as f(covariates)
-group_level %>%
-  gather(key = pred, value = xval, X1, X2, Z) %>%
-  try(
-    ggplot(aes(x = xval, y = theta_hypermean)) +
-    geom_point(aes(color = as.factor(party))) +
-    geom_smooth(aes(color = as.factor(party)), method = "lm") +
-    facet_wrap(~ pred)
-  )
 
-group_level %>%
-  gather(key = pred, value = xval, X1, X2, Z) %>%
-  try(
-    ggplot(aes(x = xval, y = sigma_hypermean)) +
+# hypermeans as f(covariates)
+if (whoami == "michaeldecrescenzo") {
+  group_level %>%
+  gather(key = pred, value = xval, X1, X2, Z) %>% 
+  ggplot(aes(x = xval, y = theta_hypermean)) +
     geom_point(aes(color = as.factor(party))) +
     geom_smooth(aes(color = as.factor(party)), method = "lm") +
     facet_wrap(~ pred)
-  )
+}
 
 
 # group params as f(hypermeans)
-try(ggplot(group_level, aes(x = theta_hypermean, y = theta_g)) +
-  geom_point())
+if (whoami == "michaeldecrescenzo") {
+  ggplot(group_level, aes(x = theta_hypermean, y = theta_g)) +
+    geom_point(aes(color = as.factor(party))) +
+    geom_smooth(aes(color = as.factor(party)), method = "lm")
+}
 
-try(ggplot(group_level, aes(x = sigma_hypermean, y = sigma_g)) +
-  geom_point())
-
+if (whoami == "michaeldecrescenzo") {
+  ggplot(group_level, aes(x = sigma_hypermean, y = sigma_g)) +
+  geom_point(aes(color = as.factor(party))) +
+  geom_smooth(aes(color = as.factor(party)), method = "lm")
+}
 
 # ---- i-level -----------------------
 
+# the model never sees this kind of data:
+# this is INDIVIDUAL ideal point data
+# This is what creates responses, which are avg'd w/in group
+# and then the model estimates the average
+
 # draws from group data
-# we account for group variance at this point
+# we're accounting for heteroskedastic group variance at this point
 i_level <- group_level %>%
   expand(group, i_in_d = 1:n_cases) %>%
-  left_join(select(group_level, region, state, party, district, group, theta_g, sigma_g)) %>%
-  mutate(id = 1:n(), 
-         theta_i = rnorm(n(), mean = theta_g, sd = sigma_g)) %>%
-  # left_join(group_level) %>%
+  left_join(
+    group_level %>%
+      select(region, state, party, district, group, theta_g, sigma_g)
+  ) %>%
+  mutate(
+    id = 1:n(), 
+    theta_i = rnorm(n(), mean = theta_g, sd = sigma_g)
+  ) %>%
   select(id, i_in_d, district, state, region, party, group, theta_i) %>%
   # select(state, party, district, group, X1, X2, Z, theta_hypermean, theta_g, sigma_g, i_in_d, theta_i, id) %>%
   print()
+
+if (whoami == "michaeldecrescenzo") {
+  ggplot(i_level) +
+  aes(x = theta_i, color = as.factor(party)) +
+  facet_wrap(~ party) +
+  geom_density(aes(group = group), fill = NA, alpha = 0.5)
+}
+
+
+
 
 # ---- item level -----------------------
 
 # normal cutpoint, positive (log-normal) slopes
 item_level <- 
-  tibble(item = as.numeric(1:n_items), 
-         cutpoint = rnorm(n_items, mean = 0, sd = 0.1), 
-         discrimination = rlnorm(n_items, mean = -0.75, sd = 0.35), 
-         dispersion = (1 / discrimination)) %>%
+  tibble(
+    item = as.numeric(1:n_items), 
+    cutpoint = rnorm(n_items, mean = 0, sd = difficulty_sd), 
+    disc_raw = rlnorm(n_items, meanlog = 0, sd = discrimination_sd),
+    discrimination = 
+      log(disc_raw) %>%
+      sum() %>%
+      exp() %>%
+      (function(x) disc_raw * x^(-1 / n_items)),
+    # disc_raw * exp(sum(log(disc_raw)))^(-1 / n_items),
+    # rlnorm(n_items, mean = -0.75, sd = 0.35), 
+    dispersion = (1 / discrimination)
+  ) %>%
   print()
+
+item_level %$% prod(disc_raw)
+item_level %$% prod(discrimination)
 
 
 # ---- response data -----------------------
@@ -196,26 +295,37 @@ ij_level <-
   crossing(id = i_level$id, item = item_level$item) %>%
   left_join(i_level) %>%
   left_join(item_level) %>%
-  mutate(mean = (theta_i - cutpoint) / dispersion,
-         pi_ij = pnorm(mean),
-         y_draw = as.numeric(rbernoulli(n = n(), p = pi_ij)),
-         error = rnorm(n()),
-         y_cut = as.numeric((mean + error) > 0)) %>%
-  select(id, item, i_in_d, district, state, region, party, group, pi_ij, y_cut) %>%
+  mutate(
+    mean = (theta_i - cutpoint) / dispersion, 
+    pi_ij = plogis(mean), 
+    y_draw = as.numeric(rbernoulli(n = n(), p = pi_ij)), 
+    error = rlogis(n()), 
+    y_cut = as.numeric((mean + error) > 0)
+  ) %>%
+  select(id, item, i_in_d, district, state, region, party, group, theta_i, pi_ij, y_cut) %>%
   print()
 
 
+if (whoami == "michaeldecrescenzo") {
+  ggplot(ij_level) +
+  aes(x = theta_i, y = pi_ij) +
+  geom_point()
+}
 
 
 # ---- grouped response data -----------------------
 
 grouped_responses <- ij_level %>%
   group_by(region, state, district, party, group, item) %>%
-  summarize(pi_bar = mean(pi_ij),
-            y = sum(y_cut),
-            trials = n()) %>%
+  summarize(
+    pi_bar = mean(pi_ij), 
+    y = sum(y_cut), 
+    trials = n()
+  ) %>%
   ungroup() %>%
   print()
+
+
 
 
 
@@ -230,6 +340,11 @@ model_data <- grouped_responses %>%
 
 
 
+if (whoami == "michaeldecrescenzo") {
+  ggplot(model_data) +
+    aes(x = theta_g, y = pi_bar) +
+    geom_point()
+}
 
 
 # ----------------------------------------------------
@@ -305,8 +420,9 @@ long_het <-
   stan_model(stanc_ret = ., verbose = TRUE) %>%
   print()
 
-try(beepr::beep(2))
-
+if (whoami == "michaeldecrescenzo") {
+  beepr::beep(2)
+}
 
 
 long_homsk
