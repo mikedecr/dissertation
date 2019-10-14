44fd920184ea947aa1745e41930f981f925ca43f
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2019-10-14 13:13:38 -0500

    Consolidate helpers into knitr-helpers

--- assets-bookdown/knitr-helpers.R
+++ assets-bookdown/knitr-helpers.R
@@ -2,6 +2,12 @@
 #   Knit/paper-writing assistance
 # ----------------------------------------------------
 
+# contains:
+# - chunks opts, graphics device (redundant with _output?)
+# - functions (include rmd, smarter_number)
+# - call other helpers [graphics theme, symlink data]
+
+
 # Default chunk options for all chapters
 # file stems are appended to cache and fig paths
 knitr::opts_chunk$set(
@@ -25,14 +31,29 @@ include_text <- function(input, sep = "\n\n  "){
 
 # smarter number functions
 smart_number <- function(n, ...) {
+  # if non-int below ten, return as is
   if ((n == as.integer(n)) == FALSE) {
     return(n)
   } else 
+  # if non-int above ten, return number()
   if (abs(n) >= 10) {
     return(scales::number(n, big.mark = ",", ...))
   } else 
+  # if int below 10, print english
   if (abs(n) < 10) {
     return(english::english(n, ...))
   } else 
   stop("Something is wrong with this number")
 }
+
+
+# ---- call other helpers -----------------------
+
+# to do: (?) 
+#  - symlink-data redundantly loads libs; eval in separate env?
+
+source(here::here("code", "helpers", "graphics-helpers.R"))
+source(here::here("code", "helpers", "symlink-data.R"))
+
+
+
